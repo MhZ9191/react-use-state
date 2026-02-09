4887c6d11a65e239ed7757ac515e2dba268b1aa8
MhZ9191 <hegic91@gmail.com>
2026-02-09 17:03:32 +0100

    Added test mouse event

--- src/components/CardLanguage.jsx
+++ src/components/CardLanguage.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 export default function Cardlanguage({ title, clickCard, isActive, index }) {
+  const [over, setOver] = useState(false);
   return (
     <>
       <div
-        className={
-          isActive === index ? "languages-div active" : "languages-div"
-        }
+        className={`
+            languages-div
+          ${isActive === index ? "active" : ""}
+          ${over ? "active" : ""}
+          `}
         onClick={clickCard}
+        onMouseEnter={() => setOver(true)}
+        onMouseLeave={() => setOver(false)}
       >
         {title}
       </div>
