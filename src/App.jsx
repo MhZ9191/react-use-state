dbb108acb7f7c9e17e1c14e6eeacf4eea297676c
MhZ9191 <hegic91@gmail.com>
2026-02-09 14:53:26 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import Languages from "./assets/data/Languages";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <>
+      <Header title="Learn Web Development" />
+    </>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header({ title }) {
+  return (
+    <>
+      <header>
+        <div className="header-title">
+          <h1>{title}</h1>
+        </div>
+      </header>
+      ;
+    </>
+  );
+}
